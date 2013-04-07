163411ffc9d02471c739b6c361ce9455dce47d11
katiepru <katiepru@nbcs.rutgers.edu>
2013-04-07 04:47:18 +0000

    adding basic socket things

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express'),
+  app = express(),
+  server = require('http').createServer(app),
+  io = require('socket.io').listen(server),
+  currentValue = 0;
+
+app.get('/', function(req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+
+io.sockets.on('connection', function(socket) {
+  currentValue += 1;
+  socket.emit('data', currentValue);
+});
+
+server.listen(3000);
